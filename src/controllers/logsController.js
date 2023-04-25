69010e0ba0492f59c088f0d4e1ec21127e24f979
ammarzeer <ammar.alzeer@pwc.com>
2023-04-25 10:26:03 +0300

    implementing logs pagination

--- src/controllers/logsController.js
+++ src/controllers/logsController.js
@@ -1,7 +1,14 @@
 const Logs = require("../database/models/logsModel");
+const { DEFAULT_LOGS_PAGINATION } = require("../utils/enums");
 async function getAllLogs(req, res, next) {
+  const {
+    page = DEFAULT_LOGS_PAGINATION.page,
+    limit = DEFAULT_LOGS_PAGINATION.limit,
+  } = req.body.pagination;
   try {
-    const logs = await Logs.find();
+    const logs = await Logs.find()
+      .skip(page * limit)
+      .limit(limit);
     res.status(200).json(logs);
   } catch (e) {
     next({ status: 200, messgae: e.message });
--- src/utils/enums.js
+++ src/utils/enums.js
@@ -5,6 +5,12 @@ const API_TYPE = {
   PUT: "PUT",
 };
 
+const DEFAULT_LOGS_PAGINATION = {
+  page: 0,
+  limit: 5,
+};
+
 module.exports = {
   API_TYPE,
+  DEFAULT_LOGS_PAGINATION,
 };
